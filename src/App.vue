f44d6579751cf01f78cdb7651ea04150984d5ced
konraddymon <konrad.dymon@gmail.com>
2021-10-25 22:02:14 +0200

    Recruitment Task

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container mx-auto">
+      <component-a v-for="n in 3" :key="n"></component-a>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ComponentA from './components/ComponentA/ComponentA.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ComponentA
   }
 }
 </script>
@@ -21,8 +22,9 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background: rgb(100,113,112);
+  background: linear-gradient(180deg, rgba(100,113,112,1) 0%, rgba(9,12,21,1) 100%);
+
 }
 </style>
---
+++ src/components/ComponentB/ComponentB.vue
@@ -0,0 +1,3 @@
+
+<script src="./ComponentB.js"></script>
+<style lang="scss" src="./ComponentB.scss"></style>
